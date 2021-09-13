67553f76eb5a349a2d16867fe71d963762cf15d6
TonyAhn <ejrrb8@gmail.com>
2021-09-13 23:24:30 +0900

    Java review

---
+++ oracle_classes/src/only_final_in_local_static.java
@@ -0,0 +1,10 @@
+    public void sayGoodbyeInEnglish() {
+        class EnglishGoodbye {
+            public static final String farewell = "Bye bye";
+            public void sayGoodbye() {
+                System.out.println(farewell);
+            }
+        }
+        EnglishGoodbye myEnglishGoodbye = new EnglishGoodbye();
+        myEnglishGoodbye.sayGoodbye();
+    }
