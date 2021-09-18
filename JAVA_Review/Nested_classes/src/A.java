35db171573c723d59bd52272ce46f10ca0bacd52
TonyAhn <ejrrb8@gmail.com>
2021-09-18 17:22:07 +0900

    java

--- JAVA_Review/Nested_classes/src/A.java
+++ JAVA_Review/Nested_classes/src/A.java
@@ -18,6 +18,35 @@ class A_1
 			method2();
 		}
 	}
+	
+	static void outMethod(final int arg1, int arg2)
+	{
+		final int var1 = 1;
+		int var2 = 2;
+		class LocalClass
+		{
+			void method()
+			{
+				int result = arg1 + arg2 + var1 + var2;
+			}
+		}
+	}
+	
+	// 실제 :
+	class LocalClass
+	{
+		int arg2 = 매개값; // 필드로 복사
+		int var2 = 2; // 필드로 복사
+		
+		void method()
+		{
+			int arg1 = 매개값; // 로컬 변수로 복사 (final)
+			int var1 = 1; // 로컬 변수로 복사 (final)
+			int result = arg1 + arg2 + var1 + var2;
+		}
+	}
+	
+	
 }
 
 
